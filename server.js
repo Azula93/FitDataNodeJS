1d113db04104dbfbdfb52e5f438d4ad8ff249ee4
Azula Dev <azuladev93@gmail.com>
2024-12-09 19:06:20 -0500

    CAMBIANDO PUERTO

--- server.js
+++ server.js
@@ -89,5 +89,5 @@ app.delete('/eliminar-dato/:id', authController.isAuthenticated, (req, res) => {
 
 
 // conexion al puerto
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
