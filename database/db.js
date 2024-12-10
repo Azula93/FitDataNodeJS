54babc03d891b5bb78fc9f2c6cd75cc9c2cee5d8
Azula Dev <azuladev93@gmail.com>
2024-12-10 18:35:44 -0500

    prueba conexion db

--- database/db.js
+++ database/db.js
@@ -6,7 +6,10 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'Azula93!$%',
-  database: process.env.DB_DATABASE || 'fitdataDB'
+  database: process.env.DB_DATABASE || 'fitdataDB',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 connection.connect((err) => {
