21ccfd36922d9c6d822d4d1835ec4f78345db3fe
Azula Dev <azuladev93@gmail.com>
2024-12-09 21:01:34 -0500

    MODIFICACION conexion db

--- database/db.js
+++ database/db.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config({path: './env/.env'});
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_DATABASE
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'Azula93!$%',
+  database: process.env.DB_DATABASE || 'fitdataDB'
 });
 
 connection.connect((err) => {
